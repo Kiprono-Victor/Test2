f88c8e8bc8ad18d8e81378a2e92e506a6b79a32d
VICTOR <kipronov98@gmail.com>
2020-02-03 22:40:31 -0800

    v1

---
+++ src/nyiks1.kt
@@ -0,0 +1,10 @@
+import javafx.scene.layout.BorderRepeat
+
+fun main(args:Array<String>){
+    var name:String
+    name = "Bore"
+    display(name)
+}
+fun display(name: String){
+    println(name)
+}
